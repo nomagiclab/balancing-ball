b31bb7c46a87839793e760cc32d70563f58c1ea8
Mateusz <matiutd888@gmail.com>
2021-12-05 02:12:38 +0100

    Change camelCase to snake_case where appropriate

--- utils/button.py
+++ utils/button.py
@@ -2,13 +2,13 @@ import pybullet as p
 
 
 class Button:
-    def __init__(self, buttonId: int):
-        self.id = buttonId
+    def __init__(self, button_id: int):
+        self.id = button_id
         self.counter = 0
 
     # returns True if button was clicked since the last time
     # wasClicked returned True.
-    def wasClicked(self) -> bool:
+    def was_clicked(self) -> bool:
         if p.readUserDebugParameter(self.id) > self.counter:
             self.counter = p.readUserDebugParameter(self.id)
             return True

fa544398279a3059d5bca1e983bc457a687a13df
Mateusz <matiutd888@gmail.com>
2021-12-09 00:44:19 +0100

    Type pybullet parameter

--- utils/pid_performer.py
+++ utils/pid_performer.py
@@ -2,13 +2,14 @@ from ball.abc_ball import ABCBall
 from controllers.pid_balancer import OUT_OF_RANGE
 from paddle.abc_paddle import ABCPaddle
 from utils.environment import init_standard_pid_tools
+import pybullet
 
 
 class PidPerformer:
-    def __init__(self, pybullet_client, ball: ABCBall, paddle: ABCPaddle):
+    def __init__(self, pybullet_client: pybullet, ball: ABCBall, paddle: ABCPaddle):
         self.pybullet_client = pybullet_client
-        self.pid_sliders, self.pid_button, self.pid_balancer = init_standard_pid_tools(pybullet_client, ball, paddle,
-                                                                                       45, -45)
+        self.pid_sliders, self.pid_button, self.pid_balancer = init_standard_pid_tools(
+            pybullet_client, ball, paddle, 45, -45)
         self.paddle = paddle
 
     def perform_pid_step(self):

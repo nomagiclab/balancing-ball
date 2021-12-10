abefa550429eba372342a18c81979628504512ca
dmkwis <dw418484@students.mimuw.edu.pl>
2021-12-10 22:16:25 +0100

    Finished color thresholding.

--- src/realsense_experiment.py
+++ src/realsense_experiment.py
@@ -7,6 +7,7 @@ import numpy as np
 import os
 
 from vision.realsense import UsbRealsenseCamera
+from segmentation.segmentation import blurred_thresholding
 
 
 
@@ -17,7 +18,7 @@ COLOR_MAX = np.array([140, 200, 200],np.uint8)
 
 camera = UsbRealsenseCamera()
 
-os.mkdir(dir_name)
+os.makedirs(dir_name, exist_ok=True)
 
 def convert(t: np.ndarray):
     return torch.Tensor(t).permute(2, 0, 1)/255
@@ -25,22 +26,15 @@ def convert(t: np.ndarray):
 for i in range(1):
     color, depth = camera.raw_photo(colorized_depth = True)
 
-    blurred = cv.blur(color, (10, 10))
-    
-    hsv = cv.cvtColor(blurred, cv.COLOR_RGB2HSV)
-    frame_threshed = cv.inRange(hsv, COLOR_MIN, COLOR_MAX)
+    frame_threshed = blurred_thresholding(color)
     frame_threshed = np.expand_dims(frame_threshed, axis=-1)
-    print(frame_threshed.shape)
-    print(blurred.shape)
 
     color = convert(color)
-    blurred = convert(blurred)
     frame_threshed = convert(frame_threshed)
 
     depth = torch.Tensor(depth).permute(2, 0, 1)
 
     save_image(color, dir_name +  f'color{i}.png')
-    save_image(blurred, dir_name + f'blurred{i}.png')
     save_image(depth, dir_name +  f'depth{i}.png')
     save_image(frame_threshed, dir_name + f'frame_threshed{i}.png')
     time.sleep(1)

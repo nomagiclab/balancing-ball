01419418afc7441c99682824d8464f23a13dfdf0
dmkwis <dw418484@students.mimuw.edu.pl>
2022-01-08 21:41:09 +0100

    Add second experiment

--- src/realsense_experiment.py
+++ src/realsense_experiment.py
@@ -20,19 +20,8 @@ os.makedirs(dir_name, exist_ok=True)
 def convert(t: np.ndarray):
     return torch.Tensor(t).permute(2, 0, 1)/255
 
-for i in range(1):
+while True:
     color, depth = camera.raw_photo()
 
     frame_threshed = blurred_thresholding(color)
     print("DISTANCE:", calculate_distance(frame_threshed, depth))
-    frame_threshed = np.expand_dims(frame_threshed, axis=-1)
-
-    color = convert(color)
-    frame_threshed = convert(frame_threshed)
-
-    depth = torch.Tensor(depth)/np.average(depth)
-
-    save_image(color, dir_name +  f'color{i}.png')
-    save_image(depth, dir_name +  f'depth{i}.png')
-    save_image(frame_threshed, dir_name + f'frame_threshed{i}.png')
-    time.sleep(1)

9f65ebcee06ce96fd210f7b84b020f503417b23c
dmkwis <dw418484@students.mimuw.edu.pl>
2022-03-19 16:49:50 +0100

    Linter.

--- src/gui.py
+++ src/gui.py
@@ -1,7 +1,6 @@
 import torch
 from torchvision.utils import save_image
 import cv2 as cv
-import time 
 import numpy as np
 
 import os
@@ -12,16 +11,20 @@ from segmentation.segmentation import blurred_thresholding, bitmask_average
 camera = UsbRealsenseCamera()
 
 while True:
-	rgb, _, _ = camera.take_photo()
-	frame_thresholded = blurred_thresholding(rgb)
-	rgb = cv.cvtColor(rgb, cv.COLOR_RGB2BGR)
-	bit_average = bitmask_average(frame_thresholded)
-	frame_thresholded = np.dstack((frame_thresholded, frame_thresholded, frame_thresholded))
-	if bit_average is not None:
-		avg_x, avg_y = bit_average
-		circle_centre = int(avg_x), int(avg_y)
-		frame_thresholded = cv.circle(frame_thresholded, circle_centre, 5, (0,0,255), -1)
-	cv.imshow('rgb vision', rgb)
-	cv.imshow('th vision', frame_thresholded)
-	
-	cv.waitKey(5) # waits until a key is pressed
+    rgb, _, _ = camera.take_photo()
+    frame_thresholded = blurred_thresholding(rgb)
+    rgb = cv.cvtColor(rgb, cv.COLOR_RGB2BGR)
+    bit_average = bitmask_average(frame_thresholded)
+    frame_thresholded = np.dstack(
+        (frame_thresholded, frame_thresholded, frame_thresholded)
+    )
+    if bit_average is not None:
+        avg_x, avg_y = bit_average
+        circle_centre = int(avg_x), int(avg_y)
+        frame_thresholded = cv.circle(
+            frame_thresholded, circle_centre, 5, (0, 0, 255), -1
+        )
+    cv.imshow("rgb vision", rgb)
+    cv.imshow("th vision", frame_thresholded)
+
+    cv.waitKey(5)  # waits until a key is pressed

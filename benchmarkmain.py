a2e0815e69cfe63741d992b333070b1a6f81ab75
matiutd888 <34310650+matiutd888@users.noreply.github.com>
2022-04-23 16:19:33 +0200

    Refactor benchmark (#52)

--- benchmarkmain.py
+++ benchmarkmain.py
@@ -1,3 +1,6 @@
+import argparse
+import os
+
 from position_prediction.benchmark import Benchmark
 import pybullet as p
 
@@ -6,23 +9,43 @@ from position_prediction.polynomial_interpolation import PolynomialPredicter
 from trackers.concurrent_ball_tracker import ConcurrentPredictingBallTracker
 from utils.environment import init_env_and_load_assets
 
-ball_controller, ball, paddle, wind_controllers = init_env_and_load_assets(p)
+parser = argparse.ArgumentParser()
+DEFAULT_FILE_NAME = "polynomial_prediction_benchmark"
+DEFAULT_N_PREDICT = 20
+DEFAULT_FETCH_TIME = 1 / 10  # 10 is the maximum number of camera outputs per second.
 
+parser.add_argument("file_name", default=DEFAULT_FILE_NAME, type=str)
+parser.add_argument("-d", type=int, help="N_DELAYED parameter")
+parser.add_argument(
+    "-p", type=int, help="N_PREDICT parameter", default=DEFAULT_N_PREDICT
+)
+parser.add_argument(
+    "-f", type=float, help="FETCH_TIME parameter", default=DEFAULT_FETCH_TIME
+)
+parser.add_argument("--delete", action="store_true")
+args = parser.parse_args()
+
+N_DELAYED = args.d
+N_PREDICT = args.p
+
+ball_controller, ball, paddle, wind_controllers = init_env_and_load_assets(p)
 paddle.create_joint_controllers()
 
-N_DELAYED = 0
-N_PREDICT = 1
 
 predicter = PolynomialPredicter()
 tracker = ConcurrentPredictingBallTracker(
-    DelayedPybulletBall(ball, N_DELAYED), paddle, N_PREDICT, 0, predicter, 0.0001
+    DelayedPybulletBall(ball, N_DELAYED), paddle, N_PREDICT, 0, predicter, 0.1
 )
 
-FILE_NAME = "polynomial_prediction_benchmark"
-file_name = FILE_NAME + ".csv"
-plot_name = FILE_NAME + ".png"
+file_name = args.file_name
+csv_name = file_name + ".csv"
+plot_name = file_name + ".png"
 
-benchmark = Benchmark(tracker, ball, file_name, [(1.5, 1.5), (-1.5, -1.5)])
+benchmark = Benchmark(tracker, ball, csv_name, [(0.5, 0.5), (-0.5, -0.5), (-1, -1)])
 benchmark.run_benchmark(p, paddle, 6)
 
-benchmark.plot_linear(file_name, plot_name)
+benchmark.plot_linear(csv_name, plot_name)
+
+if args.delete:
+    os.remove(csv_name)
+    os.remove(plot_name)

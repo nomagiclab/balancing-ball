ca6e8df1dea53fa50c98ac3712831e8641031224
dmkwis <dw418484@students.mimuw.edu.pl>
2022-06-01 02:57:40 +0200

    No position fix.

--- position_prediction/no_prediction.py
+++ position_prediction/no_prediction.py
@@ -2,11 +2,6 @@ from typing import List
 
 from position_prediction.abc_predicter import ABCPredicter
 
-"""
-For details please visit: 
-https://www.researchgate.net/publication/334980885_Prediction_of_a_Ball_Trajectory_for_the_Humanoid_Robots_A_Friction-Based_Study
-https://en.wikipedia.org/wiki/Exponential_smoothing
-"""
 class NoPredictionPredicter(ABCPredicter):
     def __init__(self):
         pass

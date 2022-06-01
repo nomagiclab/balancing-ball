a49b9ef166d624ce9fc562cd0b401c11a4d768e9
Barrus <58426102+Barrus00@users.noreply.github.com>
2022-06-01 20:38:35 +0200

    Added the position based benchmark. (#66)
    * Added the position based benchmark.
    Changed the tracker to the real ball tracker in wind benchmark.
    
    * Performed the same changes to the force_benchmark.py, as earlier for the wind_benchmark.py.
    
    Co-authored-by: Barrus00 <b.ruszewski@lekseek.com>

--- benchmarkmain.py
+++ benchmarkmain.py
@@ -4,11 +4,11 @@ import os
 import pybullet as p
 
 from ball.delayed_pybullet_ball import DelayedPybulletBall
+from benchmarks.stpos_benchmark import StPosBenchmark
+from benchmarks.wind_benchmark import WindBenchmark
 from position_prediction.DES import DESPredicter
 from benchmarks.force_benchmark import ForceBenchmark
-from position_prediction.polynomial_interpolation import PolynomialPredicter
 from position_prediction.no_prediction import NoPredictionPredicter
-from trackers.ball_tracker import BallTracker
 from trackers.concurrent_ball_tracker import ConcurrentPredictingBallTracker
 from utils.environment import init_env_and_load_assets
 
@@ -18,9 +18,13 @@ DEFAULT_N_PREDICT = 1
 DEFAULT_N_DELAYED = 0
 DEFAULT_FETCH_TIME = 1 / 30  # 20 is the maximum number of camera outputs per second.
 
+
 parser.add_argument("--file_name", default=DEFAULT_FILE_NAME, type=str)
 parser.add_argument(
     "-d", type=int, help="N_DELAYED parameter", default=DEFAULT_N_DELAYED
+)
+parser.add_argument(
+    "--benchmark-type", type=str, help="Specify the benchmark type.", default="FORCE"
 )
 parser.add_argument(
     "-p", type=int, help="N_PREDICT parameter", default=DEFAULT_N_PREDICT
@@ -38,7 +42,7 @@ N_DELAYED = args.d
 N_PREDICT = args.p
 FETCH_TIME = args.f
 NO_PREDICTION = args.no_prediction
-
+BENCHMARK_TYPE = args.benchmark_type
 (
     ball_controller,
     ball,
@@ -62,11 +66,22 @@ file_name = args.file_name
 csv_name = file_name + ".csv"
 plot_name = file_name + ".png"
 
-benchmark = ForceBenchmark(tracker, ball, [(0.225, 0.225), (0.1, 0.1), (-0.2, 0.1)])
+if BENCHMARK_TYPE == "POSITION":
+    benchmark = StPosBenchmark(tracker, ball)
+elif BENCHMARK_TYPE == "WIND":
+    benchmark = WindBenchmark(tracker, ball)
+else:
+    benchmark = ForceBenchmark(tracker, ball, [(0.225, 0.225), (0.1, 0.1), (-0.2, 0.1)])
+
 benchmark.run_benchmark(p, paddle, 3, (400.0, 1.0, 70.0))
+
 print("SAVING PLOTS")
-benchmark.get_prediction_error(plot_name)
+
+if BENCHMARK_TYPE == "FORCE":
+    benchmark.get_prediction_error(plot_name)
+
 benchmark.plot()
+input("CONTINUE?")
 
 if args.delete:
     os.remove(csv_name)

5e79baab04da45934ba58207f9a9cc0b104e6311
Barrus <58426102+Barrus00@users.noreply.github.com>
2022-06-02 14:51:20 +0200

    Implemented a method to measure the paddle, and scale the error adequately. (#68)
    * Implemented a method to measure the paddle, and scale the error adequately.
    
    * Style changes.
    
    * Internal changes.
    
    * Internal changes.
    
    Co-authored-by: Barrus00 <b.ruszewski@lekseek.com>

---
+++ realsense_test.py
@@ -0,0 +1,10 @@
+from trackers.realsense_tracker import RealsenseTracker
+
+tracker = RealsenseTracker((200, 296), True)
+tracker.camera.measure_paddle(True)
+
+while True:
+    try:
+        print("ERROR: ", tracker.get_error_vector())
+    except:
+        print("OUT OF RANGE")
